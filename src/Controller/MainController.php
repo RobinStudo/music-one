e61cab5a261bf99b81df48280fd3eed19d3287b2
cloelhenrylh <cloelhenrylh@gmail.com>
2022-04-15 14:01:30 +0200

    exercice 1 : ajouter un evenement mis en avant + le prochain evenement pour lequel il reste de la place

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,6 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\EventRepository;
+use DateTime;
 
 /**
  * @Route("", name="main_")
@@ -15,12 +19,60 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
 
+    private EntityManagerInterface $em;
+    private EventRepository $eventRepository;
+
+    public function __construct(EntityManagerInterface $em, EventRepository $eventRepository)
+    {
+        $this->em = $em;
+        $this->eventRepository = $eventRepository;
+    }
+
     /**
      * @Route("/", name="index")
      */
     public function index(): Response
     {
-        return $this->render('main/index.html.twig');
+
+        // evenement mis en avant : 
+        $events = $this->eventRepository->findAll();
+        $mainEvent = null;
+        $isEvent = (count($events) > 0);
+        if ($isEvent){
+            $mainEvent = $events[rand(0, count($events) -1)];
+        }
+        
+        // prochain event où il y a de la place : 
+        $nextEvent = null;
+        if ($isEvent){
+            $startAt = array();
+            foreach ($events as $key => $row)
+            {
+                $startAt[$key] = $row->getStartAt();;
+            }
+            array_multisort($startAt, SORT_ASC, $events);
+            
+            $find = false;
+            foreach($events as $event){
+                if (!$find){
+                    $capacity = $event->getCapacity();
+                    $participants = count($event->getParticipants());
+                    $isPlace = ($capacity - $participants) > 0;
+                    $now = date('l jS \of F Y h:i:s A');
+                    $isDate = (new DateTime() < $event->getStartAt());
+                    if ($isPlace && $isDate){
+                        $nextEvent = $event;
+                        $find = true;
+                    }
+                }
+            }
+        }
+
+        return $this->render('main/index.html.twig', [
+            'mainEvent' => $mainEvent, 
+            'nextEvent' => $nextEvent,
+            'isEvent' => $isEvent
+        ]);
     }
 
     /**
